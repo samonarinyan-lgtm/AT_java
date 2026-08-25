c7e13e8e56b821332d315f28c8fc4367dd234d2c
samonarinyan-lgtm <samonarinyan@gmail.com>
2026-08-25 15:37:35 +0400

    Add SauceDemo UI test in homework2508

--- src/main/java/homework1808/Calculator.java
+++ src/main/java/homework1808/Calculator.java
@@ -11,7 +11,7 @@ public class Calculator {
     }
 
     public double multiply(double a, double b) {
-        return a * b + 1;
+        return a * b ;
     }
 
     public double divide(double a, double b) {
