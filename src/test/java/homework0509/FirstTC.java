8e301f206965fa4443a6b496fb483a209f9cf59e
samonarinyan-lgtm <samonarinyan@gmail.com>
2026-09-08 03:16:26 +0400

    Update FirstTC and SecondTC with complete test code

--- src/test/java/homework0509/FirstTC.java
+++ src/test/java/homework0509/FirstTC.java
@@ -1,4 +1,53 @@
 package homework0509;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+import java.util.List;
+import java.util.Random;
+
 public class FirstTC {
+    WebDriver driver;
+
+    @BeforeMethod
+    public void create() {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+
+    }
+
+    @Test
+    public void selectRandomJobTest() {
+        driver.get("https://staff.am/jobs");
+
+        List<WebElement> allJobs = driver.findElements(By.xpath("//a[contains(@href, '/jobs/')]"));
+        Random random = new Random();
+
+        int randomIndex = random.nextInt(allJobs.size());
+        WebElement randomJob = allJobs.get(randomIndex);
+        Actions actions = new Actions(driver);
+        actions.scrollToElement(randomJob).perform();
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(3));
+        wait.until(ExpectedConditions.elementToBeClickable(randomJob));
+
+        randomJob.click();
+    }
+
+    @AfterMethod
+    public void close() {
+        if (driver != null) {
+            driver.quit();
+
+        }
+    }
 }
--- src/test/java/homework0509/SecondTC.java
+++ src/test/java/homework0509/SecondTC.java
@@ -1,4 +1,75 @@
 package homework0509;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import java.time.Duration;
+import java.util.List;
+
 public class SecondTC {
+
+    WebDriver driver;
+
+
+    @BeforeMethod
+    public void create() {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+    }
+
+    @Test
+    public void testSearchField() {
+        driver.get("https://staff.am/en/jobs");
+
+        WebElement input = driver.findElement(By.name("JobsFilter[key_word]"));
+        input.sendKeys("qa");
+        WebElement btn = driver.findElement(By.className("search-btn"));
+        btn.click();
+
+        WebElement clearBtn = driver.findElement(By.xpath("//a[contains(@class, 'clear')]"));
+        Assert.assertTrue(clearBtn.isDisplayed());
+
+        List<WebElement> jobsList = driver.findElements(By.xpath("//a[contains(@href, '/jobs/view/')]"));
+        Assert.assertTrue(jobsList.size() > 0);
+
+        WebElement input2 = driver.findElement(By.name("JobsFilter[key_word]"));
+        input2.clear();
+        input2.sendKeys("developer");
+        input2.sendKeys(Keys.ENTER);
+
+        WebElement clearBtn2 = driver.findElement(By.xpath("//a[contains(@class, 'clear')]"));
+        Assert.assertTrue(clearBtn2.isDisplayed());
+
+        WebElement input3 = driver.findElement(By.name("JobsFilter[key_word]"));
+        input3.clear();
+        input3.sendKeys("barev");
+        WebElement btn2 = driver.findElement(By.className("search-btn"));
+        btn2.click();
+
+        WebElement emptyMsg = driver.findElement(By.className("empty"));
+        Assert.assertTrue(emptyMsg.isDisplayed());
+
+        WebElement input4 = driver.findElement(By.name("JobsFilter[key_word]"));
+        input4.clear();
+        input4.sendKeys(Keys.ENTER);
+
+        List<WebElement> clearBtnList = driver.findElements(By.xpath("//a[contains(@class, 'clear')]"));
+        Assert.assertEquals(clearBtnList.size(), 0);
+        List<WebElement> restJobs = driver.findElements(By.xpath("//a[contains(@href, '/jobs/view/')]"));
+        Assert.assertTrue(restJobs.size() > 0);
+    }
+
+    @AfterMethod
+    public void close() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
 }
